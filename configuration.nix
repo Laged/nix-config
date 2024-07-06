533980e6792cc27d91013a1d0bc7f22f2403f966
Laged <parkkila.matti@gmail.com>
2024-07-06 12:23:41 +0300

    add: vim git gh

--- configuration.nix
+++ configuration.nix
@@ -4,24 +4,25 @@
     <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix>
   ];
 
+  system.stateVersion = "24.05";
   time.timeZone = "Europe/Helsinki";
   i18n.defaultLocale = "en_US.UTF-8";
-
+ 
   console = {
     keyMap = "fi";
   };
-
-  programs.sway.enable = true;
 
   sound.enable = true;
   nixpkgs.config.pulseaudio = true;
   hardware.pulseaudio.enable = true;
 
+  programs.zsh.enable = true;
   environment.shells = [ pkgs.zsh ];
-
   users.defaultUserShell = pkgs.zsh;
 
   environment.systemPackages = with pkgs; [
-    zsh
-  ];
+    vim
+    git
+    gh
+];
 }
