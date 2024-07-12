81ec421787a3051231434c7434359b56ff3258b9
Laged <parkkila.matti@gmail.com>
2024-07-12 14:52:11 +0300

    rip everybody

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
     <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix>
   ];
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
   time.timeZone = "Europe/Helsinki";
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
@@ -21,17 +21,6 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-  sound.enable = true;
-  nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio.enable = true;
-
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/laged/nix-config";
-  };
-
   programs.zsh.enable = true;
   environment.shells = [ pkgs.zsh ];
   users.defaultUserShell = pkgs.zsh;
@@ -39,7 +28,5 @@
   environment.systemPackages = with pkgs; [
     vim
     git
-    gh
-    nh
 ];
 }
